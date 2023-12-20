6264429e2e10df0bbcb5fcb7d2bd1cb627a7b232
ocsoares <casoares016@gmail.com>
2023-12-19 22:01:13 -0300

    feat: add more notes to date package

--- src/studyNotes/exceptions/date/MainDate.java
+++ src/studyNotes/exceptions/date/MainDate.java
@@ -1,6 +1,7 @@
 package studyNotes.exceptions.date;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 public class MainDate {
     public static void main(String[] args) {
@@ -11,6 +12,14 @@ public class MainDate {
         System.out.println("Números de DIAS do MÊS atual: " + currentDate.lengthOfMonth());
         System.out.println("DIA atual: " + currentDate.getDayOfMonth());
         System.out.println("DIA da SEMANA atual: " + currentDate.getDayOfWeek());
+
+        System.out.println("\n");
+
+        LocalDate specificDate = LocalDate.of(2020, Month.MAY, 15);
+        System.out.println("specificDate: " + specificDate);
+        System.out.println("ANO atual da DATA ESPECÍFICA: " + specificDate.getYear());
+
+        System.out.println("\n");
 
         LocalDate currentDatePlusTwoDays = currentDate.plusDays(2);
         System.out.println("currentDatePlusTwoDays: " + currentDatePlusTwoDays);
