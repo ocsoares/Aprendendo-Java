3eee71c26c312b9dcef4f08449b30c1a3321adb3
ocsoares <casoares016@gmail.com>
2023-12-21 17:46:13 -0300

    feat: add convertingListsAndArrays package with its file

---
+++ src/studyNotes/convertingListsAndArrays/MainListsAndArrays.java
@@ -0,0 +1,33 @@
+package studyNotes.convertingListsAndArrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainListsAndArrays {
+    public static void main(String[] args) {
+        // ------- LISTA para Arrays ! -------
+        List<Integer> numbersList = new ArrayList<>();
+        numbersList.add(10);
+        numbersList.add(34);
+        numbersList.add(8);
+        numbersList.add(240);
+
+        System.out.println("numbersList: " + numbersList);
+
+        // Por questões de PERFORMANCE, é mais Recomendado usar "new Integer[0]" do que
+        // "new Integer[numbersList.size]" !!!
+        Integer[] numbersArray = numbersList.toArray(new Integer[0]);
+        System.out.println("numbersArray: " + Arrays.toString(numbersArray));
+
+        System.out.println("\n");
+
+        // ------- Arrays para LISTA ! -------
+
+        Integer[] otherNumbersArray = {20, 700, 90, 2, 30};
+        System.out.println("otherNumbersArray: " + Arrays.toString(otherNumbersArray));
+
+        List<Integer> otherNumbersList = new ArrayList<>(Arrays.asList(otherNumbersArray));
+        System.out.println("otherNumbersList: " + otherNumbersList);
+    }
+}
